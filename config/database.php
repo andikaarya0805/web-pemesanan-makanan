5243fbf4f668bc1faf576aa48dadd46ef7b6bb53
andikaarya0805 <andikaaryapratama0805@gmail.com>
2026-03-30 04:25:56 +0700

    fix: force database defaults for vercel

--- config/database.php
+++ config/database.php
@@ -99,17 +99,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'url' => getenv('DATABASE_URL') ?: env('DATABASE_URL'),
+            'host' => getenv('DB_HOST') ?: env('DB_HOST', '127.0.0.1'),
+            'port' => getenv('DB_PORT') ?: env('DB_PORT', '5432'),
+            'database' => getenv('DB_DATABASE') ?: env('DB_DATABASE', 'postgres'),
+            'username' => getenv('DB_USERNAME') ?: env('DB_USERNAME', 'postgres'),
+            'password' => getenv('DB_PASSWORD') ?: env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
